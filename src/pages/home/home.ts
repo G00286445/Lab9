b3d609fcaa38454a16e52dbda00108287e11bb07
SHAUN DALY - STUDENT <G00286445@gmit.ie>
2019-03-27 17:15:18 +0000

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+myStatus: string;
+  constructor(public navCtrl: NavController, private storage: Storage) {
 
   }
 
+  changePage() {
+    this.navCtrl.push("StatusPage");
+  }
+
+  ionViewWillEnter(){
+    this.storage.get("myStatus").then((data) => {
+      this.myStatus = data;
+    })
+  }
 }
